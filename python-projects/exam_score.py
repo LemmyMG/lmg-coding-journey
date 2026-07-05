ef703ba073eb353fa9a4cbf7df09538bb73db459
LemmyMG <lemmymgee@gmail.com>
2026-07-05 13:12:45 +0100

    examinationScoreChecker

---
+++ python-projects/exam_score.py
@@ -0,0 +1,12 @@
+exam_score = float(input("Enter trade score:  "))
+
+if exam_score >= 90:
+    print("Excellent Result!")
+elif exam_score >= 70:
+    print ("Good Result")
+elif exam_score >= 50:
+    print ("Average Result")
+else: 
+    print ("Put in more effort.")
+
+print ("Record complete.")
