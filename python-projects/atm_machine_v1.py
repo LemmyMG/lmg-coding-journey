2ddb057a681828bbd54fae9550dd774bd12166a1
LemmyMG <lemmymgee@gmail.com>
2026-07-09 23:36:58 +0100

    Student_Record_System

--- python-projects/atm_machine_v1.py
+++ python-projects/atm_machine_v1.py
@@ -2,14 +2,13 @@ balance = 1000
 
 while True:
     print("\n=== LMG BANK ===")
-    print("1. Check Balance")
+    print("\n1. Check Balance")
     print("2. Exit")
     
-    choice = float(input("Choose an option: "))
+    choice = float(input("\nChoose an option: "))
 
     if choice == 1:
         print(f"Your current balance is: ${balance}")
 
     elif choice == 2:
         print("Thank you for using LMG BANK. Goodbye!")
-        break
---
+++ python-projects/student_record_system.py
@@ -0,0 +1,12 @@
+print("---------Student Record---------")
+student_name =  "John Doe"
+student_age = 20
+student_id = "L2E-007"
+department = "Computer Science"
+score = 95.5
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student ID:", student_id)
+print("Department:", department)
+print("Score:", score)
