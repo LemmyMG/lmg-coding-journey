7466f5e4943e93b3e70b3c94fcc3364995f272c7
LemmyMG <lemmymgee@gmail.com>
2026-07-10 23:10:31 +0100

    Lotsize-size-calculator-v1

--- python-projects/risk_calculator.py
+++ python-projects/risk_calculator.py
@@ -1,9 +1,13 @@
-risk_amount = float(input("Enter risk amount: "))
-stop_loss = float(input("Enter stop loss pips: "))
+print("\n---------LOTSIZE CALCULATOR----------\n")
+
+risk_amount = float(input("Enter risk amount($): "))
+stop_loss = float(input("Enter stop loss (pips): "))
 
 dollar_per_pip = risk_amount / stop_loss
+
 lot_size = dollar_per_pip / 10
-print ("Risk Amount:", risk_amount)
+
+print ("\nRisk Amount:", risk_amount)
 print ("Stop Loss:", stop_loss)
 print ("Dollar per Pip:", "$" + str(dollar_per_pip))
-print ("Recommended Lot Size:", lot_size, "lots")
+print ("\nRecommended Lot Size:", lot_size, "lots")
