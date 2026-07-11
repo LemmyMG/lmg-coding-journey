c797a2afadac050d58e1e22d1f256e3004759f98
LemmyMG <lemmymgee@gmail.com>
2026-07-11 17:06:10 +0100

    Student-record-management-system-multiple-student-data

---
+++ python-projects/student_record_system_v4.py
@@ -0,0 +1,27 @@
+
+for student in range(3):
+    print("\n---------Student Record System---------")
+    print("\n====== Entering Student", student + 1, "======")
+
+    student_name = input("Enter Student Name: ")
+    student_age = int(input("Enter student Age: "))
+    student_id = input("Enter student ID: ")
+    department = input("Enter student Department: ")
+    score = float(input("Enter student Score: "))
+
+    print("\n------- Student Record ---------\n")
+    print("Student Name:", student_name)
+    print("Student Age:", student_age)
+    print("Student ID:", student_id)
+    print("Department:", department)
+    print("Score:", score)
+
+    if score >= 70:
+        print("Grade: Excellent")
+    elif score >= 60:
+        print("Grade: Good")
+    elif score >= 50:
+        print("Grade: Average")
+    else:
+        print("Grade: Poor")
+    print("\n-------------------------------------")
